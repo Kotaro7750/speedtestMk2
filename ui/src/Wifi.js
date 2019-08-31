8104a57dfe1e179c9a2eb9fd67b3666f351306f7
Kotaro <7750koutarou@gmail.com>
2019-08-31 23:22:10 +0900

    modify

--- ui/src/Wifi.js
+++ ui/src/Wifi.js
@@ -49,20 +49,10 @@ class Wifi extends Component {
                     this.worker = null;
                 }
                 this.setState({ ip: data[4] });
-                //I("ip").textContent = data[4];
-                //test1 = data[4];
-                //I("dlText").textContent = (status == 1 && data[1] == 0) ? "..." : data[1];
                 this.setState({ dl: data[1] });
-                //test2 = data[1];
-                //I("ulText").textContent = (status == 3 && data[2] == 0) ? "..." : data[2];
                 this.setState({ ul: data[2] });
-                //test3 = data[2];
-                //I("pingText").textContent = data[3];
                 this.setState({ ping: data[3] });
-                //test4 = data[3];
-                //I("jitText").textContent = data[5];
                 this.setState({ jitter: data[5] });
-                //test5 = data[5];
             }.bind(this);
         }
     }
